9e6044e932414e6987b0dee8969000945416085b
Eduardo Blank Dias Moutinho <eduardo_blank2@hotmail.com>
2016-08-01 18:50:12 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numero1 = new System.Windows.Forms.Button();
+            this.numero2 = new System.Windows.Forms.Button();
+            this.numero3 = new System.Windows.Forms.Button();
+            this.numero6 = new System.Windows.Forms.Button();
+            this.numero5 = new System.Windows.Forms.Button();
+            this.numero4 = new System.Windows.Forms.Button();
+            this.numero9 = new System.Windows.Forms.Button();
+            this.numero8 = new System.Windows.Forms.Button();
+            this.numero7 = new System.Windows.Forms.Button();
+            this.Conta = new System.Windows.Forms.TextBox();
+            this.Back = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.Binario = new System.Windows.Forms.Button();
+            this.numero0 = new System.Windows.Forms.Button();
+            this.Hexadecimal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numero1
+            // 
+            this.numero1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero1.Location = new System.Drawing.Point(12, 278);
+            this.numero1.Name = "numero1";
+            this.numero1.Size = new System.Drawing.Size(64, 53);
+            this.numero1.TabIndex = 0;
+            this.numero1.Text = "1";
+            this.numero1.UseVisualStyleBackColor = true;
+            this.numero1.Click += new System.EventHandler(this.numero1_Click);
+            // 
+            // numero2
+            // 
+            this.numero2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero2.Location = new System.Drawing.Point(82, 278);
+            this.numero2.Name = "numero2";
+            this.numero2.Size = new System.Drawing.Size(64, 53);
+            this.numero2.TabIndex = 1;
+            this.numero2.Text = "2";
+            this.numero2.UseVisualStyleBackColor = true;
+            this.numero2.Click += new System.EventHandler(this.numero2_Click);
+            // 
+            // numero3
+            // 
+            this.numero3.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero3.Location = new System.Drawing.Point(152, 278);
+            this.numero3.Name = "numero3";
+            this.numero3.Size = new System.Drawing.Size(64, 53);
+            this.numero3.TabIndex = 2;
+            this.numero3.Text = "3";
+            this.numero3.UseVisualStyleBackColor = true;
+            this.numero3.Click += new System.EventHandler(this.numero3_Click);
+            // 
+            // numero6
+            // 
+            this.numero6.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero6.Location = new System.Drawing.Point(152, 219);
+            this.numero6.Name = "numero6";
+            this.numero6.Size = new System.Drawing.Size(64, 53);
+            this.numero6.TabIndex = 5;
+            this.numero6.Text = "6";
+            this.numero6.UseVisualStyleBackColor = true;
+            this.numero6.Click += new System.EventHandler(this.numero6_Click);
+            // 
+            // numero5
+            // 
+            this.numero5.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero5.Location = new System.Drawing.Point(82, 219);
+            this.numero5.Name = "numero5";
+            this.numero5.Size = new System.Drawing.Size(64, 53);
+            this.numero5.TabIndex = 4;
+            this.numero5.Text = "5";
+            this.numero5.UseVisualStyleBackColor = true;
+            this.numero5.Click += new System.EventHandler(this.numero5_Click);
+            // 
+            // numero4
+            // 
+            this.numero4.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero4.Location = new System.Drawing.Point(12, 219);
+            this.numero4.Name = "numero4";
+            this.numero4.Size = new System.Drawing.Size(64, 53);
+            this.numero4.TabIndex = 3;
+            this.numero4.Text = "4";
+            this.numero4.UseVisualStyleBackColor = true;
+            this.numero4.Click += new System.EventHandler(this.numero4_Click);
+            // 
+            // numero9
+            // 
+            this.numero9.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero9.Location = new System.Drawing.Point(152, 160);
+            this.numero9.Name = "numero9";
+            this.numero9.Size = new System.Drawing.Size(64, 53);
+            this.numero9.TabIndex = 8;
+            this.numero9.Text = "9";
+            this.numero9.UseVisualStyleBackColor = true;
+            this.numero9.Click += new System.EventHandler(this.numero9_Click);
+            // 
+            // numero8
+            // 
+            this.numero8.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero8.Location = new System.Drawing.Point(82, 160);
+            this.numero8.Name = "numero8";
+            this.numero8.Size = new System.Drawing.Size(64, 53);
+            this.numero8.TabIndex = 7;
+            this.numero8.Text = "8";
+            this.numero8.UseVisualStyleBackColor = true;
+            this.numero8.Click += new System.EventHandler(this.numero8_Click);
+            // 
+            // numero7
+            // 
+            this.numero7.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero7.Location = new System.Drawing.Point(12, 160);
+            this.numero7.Name = "numero7";
+            this.numero7.Size = new System.Drawing.Size(64, 53);
+            this.numero7.TabIndex = 6;
+            this.numero7.Text = "7";
+            this.numero7.UseVisualStyleBackColor = true;
+            this.numero7.Click += new System.EventHandler(this.numero7_Click);
+            // 
+            // Conta
+            // 
+            this.Conta.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Conta.Enabled = false;
+            this.Conta.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Conta.Location = new System.Drawing.Point(12, 12);
+            this.Conta.MaxLength = 1;
+            this.Conta.MinimumSize = new System.Drawing.Size(305, 62);
+            this.Conta.Name = "Conta";
+            this.Conta.ReadOnly = true;
+            this.Conta.Size = new System.Drawing.Size(305, 62);
+            this.Conta.TabIndex = 10;
+            this.Conta.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Conta.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Back
+            // 
+            this.Back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Back.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back.Location = new System.Drawing.Point(152, 101);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(165, 53);
+            this.Back.TabIndex = 11;
+            this.Back.Text = " <==";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.Location = new System.Drawing.Point(12, 101);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(134, 53);
+            this.C.TabIndex = 12;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // Binario
+            // 
+            this.Binario.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Binario.Location = new System.Drawing.Point(222, 160);
+            this.Binario.Name = "Binario";
+            this.Binario.Size = new System.Drawing.Size(95, 112);
+            this.Binario.TabIndex = 13;
+            this.Binario.Text = "Binário";
+            this.Binario.UseVisualStyleBackColor = true;
+            this.Binario.Click += new System.EventHandler(this.Binario_Click);
+            // 
+            // numero0
+            // 
+            this.numero0.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero0.Location = new System.Drawing.Point(12, 337);
+            this.numero0.Name = "numero0";
+            this.numero0.Size = new System.Drawing.Size(204, 53);
+            this.numero0.TabIndex = 14;
+            this.numero0.Text = "0";
+            this.numero0.UseVisualStyleBackColor = true;
+            this.numero0.Click += new System.EventHandler(this.numero0_Click);
+            // 
+            // Hexadecimal
+            // 
+            this.Hexadecimal.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hexadecimal.Location = new System.Drawing.Point(222, 278);
+            this.Hexadecimal.Name = "Hexadecimal";
+            this.Hexadecimal.Size = new System.Drawing.Size(95, 112);
+            this.Hexadecimal.TabIndex = 16;
+            this.Hexadecimal.Text = "Hexa- decimal";
+            this.Hexadecimal.UseVisualStyleBackColor = true;
+            this.Hexadecimal.Click += new System.EventHandler(this.Hexadecimal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 399);
+            this.Controls.Add(this.Hexadecimal);
+            this.Controls.Add(this.numero0);
+            this.Controls.Add(this.Binario);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.Conta);
+            this.Controls.Add(this.numero9);
+            this.Controls.Add(this.numero8);
+            this.Controls.Add(this.numero7);
+            this.Controls.Add(this.numero6);
+            this.Controls.Add(this.numero5);
+            this.Controls.Add(this.numero4);
+            this.Controls.Add(this.numero3);
+            this.Controls.Add(this.numero2);
+            this.Controls.Add(this.numero1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Carkuradora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button numero1;
+        private System.Windows.Forms.Button numero2;
+        private System.Windows.Forms.Button numero3;
+        private System.Windows.Forms.Button numero6;
+        private System.Windows.Forms.Button numero5;
+        private System.Windows.Forms.Button numero4;
+        private System.Windows.Forms.Button numero9;
+        private System.Windows.Forms.Button numero8;
+        private System.Windows.Forms.Button numero7;
+        private System.Windows.Forms.TextBox Conta;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button Binario;
+        private System.Windows.Forms.Button numero0;
+        private System.Windows.Forms.Button Hexadecimal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string s = Conta.Text.Substring(0, Conta.Text.Length - 1);
+           Conta.Text = s;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numero0_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "0";
+            }
+        }
+
+        private void numero1_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "1";
+            }
+        }
+
+        private void numero2_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "2";
+            }
+        }
+
+        private void numero3_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "3";
+            }
+        }
+
+        private void numero4_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "4";
+            }
+        }
+
+        private void numero5_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "5";
+            }
+        }
+
+        private void numero6_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "6";
+            }
+        }
+
+        private void numero7_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "7";
+            }
+        }
+
+        private void numero8_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "8";
+            }
+        }
+
+        private void numero9_Click(object sender, EventArgs e)
+        {
+            if (Conta.Text.Length < 11)
+            {
+                Conta.Text += "9";
+            }
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+                Conta.Text = null;
+        }
+
+        private void Binario_Click(object sender, EventArgs e)
+        {
+            long number = Convert.ToInt64(Conta.Text);
+            MessageBox.Show(Convert.ToString(number, 2));
+        }
+
+        private void Hexadecimal_Click(object sender, EventArgs e)
+        {
+            long number = Convert.ToInt64(Conta.Text);
+            MessageBox.Show(number.ToString("X"));
+        }
+    }
+}
